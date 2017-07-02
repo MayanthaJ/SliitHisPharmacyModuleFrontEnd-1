bcc90ffbfc0985400268eead2bae0d75e8855936
Dinushanka Nayomal <Rama41222@Dinushankas-MacBook-Pro.local>
2017-07-03 05:02:33 +0530

    stock management added

--- app/controllers/stock.js
+++ app/controllers/stock.js
@@ -1,26 +1,6 @@
-app.controller('StockCtrl',function(alert,$scope, $http, $location, $routeParams){
+app.controller('StockCtrl',function(auth,alert,$scope, $http, $location, $routeParams){
 
-  $scope.fetchDrugs = function(){
 
-    $http.get('http://localhost:8888/drugs/').then(function(res){
-     if(res.status == 200){
-      if(res.data.success == true){
-
-        $scope.drugList = res.data.drugList;
-
-      }else if(res.data.success == false){
-        alert('warning', 'Oops! ',res.data.msg);
-      }
-    }else if(res.status == -1){
-      alert('danger','Oops! ', 'Connection Lost');
-    }else{
-      alert('warning', 'Oops! ',res.data.msg);
-    }
-
-  },function(err){
-    alert('danger', 'Oops! ', err);
-  });
-  }
 
   $scope.fetchBatchList = function(){
 
@@ -48,34 +28,53 @@ app.controller('StockCtrl',function(alert,$scope, $http, $location, $routeParams
 
   function clearAll(){
 
-    $scope.drugId="";
-    $scope.name="";
+    $scope.batch = null;
+    $scope.alertlevel = 0;
 
   }
 
   $scope.addStock = function(){ 
 
-
     var stock = {
-      "name": $scope.name,
-      "drugId": $scope.drugId
+
+      "DrugID": $scope.batch._id,
+      "TotalQuantity": $scope.TotalQuantity,
+      "batchID":$scope.batchID,
+      "exp":$scope.exp
 
     }
 
-    var batchQty = {
-      "name": $scope.name,
-      "drugId": $scope.drugId
-
-    }
-
+   
     $http({
       method  : 'POST',
-      url     : 'http://localhost:8888/batches/',
-      data    :  batch, 
+      url     : 'http://localhost:8888/stocks/',
+      data    :  stock, 
       headers : {'Content-Type': 'application/json'} 
+
     }).then(function(res){
-      clearAll()
+
       alert('success', 'Success! ',res.data.msg);
+//stock
+             var stockId = res.data.stock._id;
+             var batch = {
+                  "quantity": $scope.TotalQuantity,
+                  "alertlevel": $scope.alertlevel,
+                  "stockId":stockId,
+                };
+
+            $http({
+                  method  : 'PUT',
+                  url     : 'http://localhost:8888/batches/'+$scope.batchID,
+                  data    :  batch, 
+                  headers : {'Content-Type': 'application/json'} 
+                 }).then(function(res){
+                clearAll()
+                alert('success', 'Success! ',res.data.msg);
+
+              },function(err){
+                alert('danger', 'Error! ',err);
+              });
+
 
     },function(err){
       alert('danger', 'Error! ',err);
@@ -83,18 +82,6 @@ app.controller('StockCtrl',function(alert,$scope, $http, $location, $routeParams
 
 
   };
-
-  $scope.editing = function(){
-
-    $scope.showthis = true;
-  };
-  $scope.fillTextBox = function(val){
-
-    $scope.drugId = val._id;
-    $scope.drug = val;
-    $scope.showthis = false;
-  };
-
 
   var today = new Date();
   var dd = today.getDate();
